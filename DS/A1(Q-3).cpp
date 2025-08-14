55825910d03908d8699c18672b378560795a87ba
suryansh-jain-dev <sjain13_be24@thapar.edu>
2025-08-14 11:12:34 +0530

    A1(Q-3).cpp

---
+++ DS/A1(Q-3).cpp
@@ -0,0 +1,14 @@
+//3) Predict the Output of the following program:
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int i;
+int arr[5] = {1};
+for (i = 0; i < 5; i++) printf("%d",arr[i]);
+return 0;
+}
+output:
+10000
